c9bf2862e1a0bdcff5e691644bba863cb71b15d1
Richard Krikler <rk@plan2.net>
2024-09-24 15:36:26 +0200

    [TASK] Compatibility with v12

--- Classes/LinkHandling/UrlLinkHandler.php
+++ Classes/LinkHandling/UrlLinkHandler.php
@@ -13,6 +13,6 @@ class UrlLinkHandler extends \TYPO3\CMS\Recordlist\LinkHandler\UrlLinkHandler
     {
         GeneralUtility::makeInstance(PageRenderer::class)->loadRequireJsModule('TYPO3/CMS/Uri2link/BetterUrlLinkHandler');
         $this->view->assign('url', !empty($this->linkParts) ? $this->linkParts['url'] : '');
-        return $this->view->render('Url');
+        return $this->view->render('LinkBrowser/Url');
     }
 }
