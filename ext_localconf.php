371095d81105c72960e5e37bbb38a69469b2023f
Richard Krikler <rk@plan2.net>
2025-05-20 14:30:02 +0200

    [TASK] Move TSConfig from ext_localconf.php ExtensionManagementUtility::addPageTSConfig to auto-imported page.tsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,16 +1,6 @@
 <?php
 
 use GeorgRinger\Uri2Link\Hooks\DataHandlerHook;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['uri2link'] =
     DataHandlerHook::class;
-
-ExtensionManagementUtility::addPageTSConfig('
-    TCEMAIN.linkHandler {
-        url {
-            handler = GeorgRinger\Uri2Link\LinkHandling\UrlLinkHandler
-        }
-
-    }
-');
